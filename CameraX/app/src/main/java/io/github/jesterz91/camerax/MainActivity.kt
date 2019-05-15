be9dbc64b27fc57d1078da64ceae321db1430a9d
jesterz91 <jesterz91@naver.com>
2019-05-15 21:30:46 +0900

    AutoFitPreviewBuilder

--- CameraX/app/src/main/java/io/github/jesterz91/camerax/MainActivity.kt
+++ CameraX/app/src/main/java/io/github/jesterz91/camerax/MainActivity.kt
@@ -2,14 +2,16 @@ package io.github.jesterz91.camerax
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.graphics.Matrix
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Rational
 import android.util.Size
-import android.view.Surface
-import android.view.ViewGroup
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.*
+import androidx.camera.core.CameraX
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureConfig
+import androidx.camera.core.PreviewConfig
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -25,49 +27,50 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
     private val REQUEST_CODE_PERMISSIONS = 10
     private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
 
+    private val FLAGS_FULLSCREEN = View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         // 카메라 권한체크
         if (allPermissionsGranted()) {
             viewFinder.post {
+                viewFinder.systemUiVisibility = FLAGS_FULLSCREEN
                 startCamera()
             }
         } else {
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
 
-        // 레이아웃 변경 반영
-        viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
-            updateTransform()
-        }
     }
 
     private fun startCamera() {
+        val metrics = DisplayMetrics().also {
+            viewFinder.display.getRealMetrics(it)
+        }
+        val screenSize = Size(metrics.widthPixels, metrics.heightPixels)
+        val screenAspectRatio = Rational(metrics.widthPixels, metrics.heightPixels)
+
         // 카메라 미리보기 설정
         val previewConfig = PreviewConfig.Builder().apply {
-            setTargetAspectRatio(Rational(1, 1))
-            setTargetResolution(Size(640, 640))
+            setTargetResolution(screenSize)
+            setTargetAspectRatio(screenAspectRatio)
         }.build()
 
         // 프리뷰 객체 생성
-        val preview = Preview(previewConfig)
-
-        // 뷰 파인더가 업데이트 될 때마다 레이아웃을 다시 계산
-        preview.setOnPreviewOutputUpdateListener {
-            // SurfaceTexture 를 업데이트하려면 제거하고 다시 추가
-            val parent = viewFinder.parent as ViewGroup
-            parent.removeView(viewFinder)
-            parent.addView(viewFinder, 0)
-            viewFinder.surfaceTexture = it.surfaceTexture
-            updateTransform()
-        }
+        // AutoFitPreviewBuilder로 크기 및 방향 변경 사항을 자동으로 처리
+        val preview = AutoFitPreviewBuilder.build(previewConfig, viewFinder )
 
         // 이미지 캡처 설정
         val imageCaptureConfig = ImageCaptureConfig.Builder()
             .apply {
-                setTargetAspectRatio(Rational(1, 1))
-                // 가로 세로 비율과 요청 된 모드에 기반한 해상도
+                setTargetAspectRatio(screenAspectRatio)
                 setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
             }.build()
 
@@ -81,7 +84,7 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
             imageCapture.takePicture(file,
                 object : ImageCapture.OnImageSavedListener {
                     override fun onError(error: ImageCapture.UseCaseError, message: String, exc: Throwable?) {
-                        val msg = "사진 캡처 실패 : $message"
+                        val msg = "사진 저장 실패 : $message"
                         toast(msg)
                         error { msg }
                         exc?.printStackTrace()
@@ -96,25 +99,6 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
         }
         // 생명주기 바인딩
         CameraX.bindToLifecycle(this, preview, imageCapture)
-    }
-
-    private fun updateTransform() {
-        val matrix = Matrix()
-        // 뷰 파인더의 중심 계산
-        val centerX = viewFinder.width / 2f
-        val centerY = viewFinder.height / 2f
-
-        // 디스플레이 회전을 고려하여 미리보기 출력을 수정
-        val rotationDegrees = when(viewFinder.display.rotation) {
-            Surface.ROTATION_0 -> 0
-            Surface.ROTATION_90 -> 90
-            Surface.ROTATION_180 -> 180
-            Surface.ROTATION_270 -> 270
-            else -> return
-        }
-        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
-        // TextureView 에 적용
-        viewFinder.setTransform(matrix)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
